a94b7a1700e20808073183d3eda260ae1f8fc4f7
potistudio <potistudio2022@gmail.com>
2025-03-25 10:58:46 +0900

    👍 feat: smart rendering & 32bpc

--- src/p4ae.cc
+++ src/p4ae.cc
@@ -33,7 +33,7 @@ static PF_Err GlobalSetup (PF_InData *in_data, PF_OutData *out_data, PF_ParamDef
 	);
 
 	out_data->out_flags = PF_OutFlag_DEEP_COLOR_AWARE | PF_OutFlag_PIX_INDEPENDENT;
-	out_data->out_flags2 = PF_OutFlag2_SUPPORTS_THREADED_RENDERING | PF_OutFlag2_SUPPORTS_SMART_RENDER;
+	out_data->out_flags2 = PF_OutFlag2_SUPPORTS_THREADED_RENDERING | PF_OutFlag2_SUPPORTS_SMART_RENDER | PF_OutFlag2_FLOAT_COLOR_AWARE;
 
 	return error;
 }
